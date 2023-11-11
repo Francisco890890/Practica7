68c0a1dfff6fdd25c9c9200a64f2f2d6917910fc
Francisco890890 <elpanaderoconelpan456@gmail.com>
2023-11-11 13:17:55 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Practica_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            pictureBox1 = new PictureBox();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(38, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(641, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Este programa imprime numeros de un rango especifico y calcula la suma";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(443, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Rango superior";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(48, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(82, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Rango inferior";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(301, 75);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 259);
+            listBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.MediumSpringGreen;
+            button1.Location = new Point(137, 235);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 31);
+            button1.TabIndex = 4;
+            button1.Text = "Imprime";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(137, 84);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(105, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(536, 79);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(105, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(517, 148);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(220, 156);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(350, 387);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(289, 390);
+            label4.Name = "label4";
+            label4.Size = new Size(37, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Suma";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Cyan;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Suma";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ListBox listBox1;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private PictureBox pictureBox1;
+        private TextBox textBox3;
+        private Label label4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,29 @@
+namespace Practica_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rangoinferior, rangosuperior, suma = 0;
+            rangoinferior = int.Parse(textBox1.Text);
+            rangosuperior = int.Parse(textBox2.Text);
+            for (int i = rangoinferior; i <= rangosuperior;)
+            {
+                listBox1.Items.Add(i);
+                suma = (suma + i);
+                i++;
+            }
+            textBox3.Text = suma.ToString();
+        }
+    }
+}
